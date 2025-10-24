dff2816799bddb76226631f15e021f6f2b5aa23a
weather027 <apimet7442@gmail.com>
2025-10-24 22:39:34 +0700

    Rebuild web and update docs for GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "bf35a15036bbaac845fd1f805928462c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "1e476506d2dedf747108aa29362d506a",
+"flutter_bootstrap.js": "775fe98a7388951d8c580758dd25588e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
